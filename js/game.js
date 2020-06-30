a1aa7ee5edded48575e93bf35a89d3a5738f8924
fagnerdev.souza <fagner.cicero@gmail.com>
2020-06-30 12:44:59 -0300

    atualizarcao

--- js/game.js
+++ js/game.js
@@ -66,16 +66,16 @@ async function verificarVencedor(){
 
     if ( ( (a1 == a2 && a1 == a3) || (a1 == b1 && a1 == c1) || (a1 == b2 && a1 == c3) ) && a1 !=""){
         if(a1 == "x"){
-            vencedor = player1.nome;
-        }else  vencedor = player2.nome;
+            vencedor = player1;
+        }else  vencedor = player2;
     } else if ( ( (b2 == b1 && b2 == b3)||(b2 == a2 && b2 == c2)||(b2 == a3 && b2 == c1) ) && b2 !=""){
         if(b2 == "x"){
-            vencedor = player1.nome;
-        }else  vencedor = player2.nome;
+            vencedor = player1;
+        }else  vencedor = player2;
     } else if ( ( (c3 == c1 && c3 == c2)||(c3 == a3 && c3 == b3) ) && c3 !=""){
         if(c3 == "x"){
-            vencedor = player1.nome;
-        }else  vencedor = player2.nome;
+            vencedor = player1;
+        }else  vencedor = player2;
     } else if((a1 != "" && a2 != "" && a3 != "" && b1 !="" && b2 !="" && b3 != "" && c1 != "" && c2 !="" && c3 !="" && vencedor == "")) {
         await sleep(50);
        
@@ -83,12 +83,12 @@ async function verificarVencedor(){
     }
     if (vencedor != ""){
         gameOver = true;
-        
+        vencedor.pontuacao+=1
         await sleep(50);
-        alert("O vencedor foi: '"+ vencedor + "'", location.reload())
+        alert("O vencedor foi: '"+ vencedor.nome + "'", location.reload())
+        console.log(vencedor.pontuacao)
 
     }
-
 }
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
